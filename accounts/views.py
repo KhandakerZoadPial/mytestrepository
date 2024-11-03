72f04ba0916a83dc4f8780b7eeaa5c2d8d2daa3c
unknown <khpial27@gmail.com>
2024-11-03 18:21:15 +0600

    authentication added

---
+++ accounts/views.py
@@ -0,0 +1,48 @@
+# In myapp/views.py
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+
+
+@api_view(['POST'])
+def signup(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if not username or not password:
+        return Response({'error': 'Username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    if User.objects.filter(username=username).exists():
+        return Response({'error': 'Username already exists.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User()
+    user.username = username
+    user.set_password(password)
+    user.save()
+
+    # Generate JWT token
+    refresh = RefreshToken.for_user(user)
+    return Response({'refresh_token': str(refresh),'access_token': str(refresh.access_token),}, status=status.HTTP_201_CREATED)
+        
+        
+@api_view(['POST'])
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    if not username or not password:
+        return Response({'error': 'Username and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        # Generate JWT token
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }, status=status.HTTP_200_OK)
+    else:
+        return Response({'error': 'Invalid credentials.'}, status=status.HTTP_401_UNAUTHORIZED)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 def home(request):
     return render(request, 'mainapp/home.html')
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def authenticated_view(request):
+    return Response({'message': 'Success! You are authenticated.'}, status=status.HTTP_200_OK)
